dc5f4b1d6e7c982ea50cea41fd604a84b3fe8155
Vitaly Obrubov <obrubov@yandex.ru>
2022-03-31 07:41:25 +0000

    Listen ports fin

--- listenports/models.py
+++ listenports/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Trackers(models.Model):
+    tracker_id = models.IntegerField(verbose_name='ИД трекера')
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Пользователь') 
+    description =  models.TextField(null=True, blank=True, verbose_name='Описание')
+    resend = models.BooleanField(default=False, verbose_name='Переслать на livegpstracks.com') 
+    class Meta:
+        verbose_name_plural = 'Трекеры' 
+        verbose_name = 'Трекер' 
+
+
 
 class Tracks(models.Model):
-    tracker_id = models.IntegerField()
-    lon = models.FloatField(null=True, blank=True)  
-    lat = models.FloatField(null=True, blank=True)
-    alt = models.FloatField(null=True, blank=True) 
-    speed = models.FloatField(null=True, blank=True)
-    accuracy = models.FloatField(null=True, blank=True)
-    bearing = models.FloatField(null=True, blank=True)
-    timestamp = models.DateTimeField()
+    tracker_m = models.ForeignKey(Trackers, on_delete=models.DO_NOTHING, null=True, verbose_name='Трекер')
+    tracker_id = models.IntegerField(verbose_name='ИД трекера')
+    lon = models.FloatField(null=True, blank=True, verbose_name='Долгота')  
+    lat = models.FloatField(null=True, blank=True, verbose_name='Широта')
+    alt = models.FloatField(null=True, blank=True, verbose_name='Высота') 
+    speed = models.FloatField(null=True, blank=True, verbose_name='Скорость')
+    accuracy = models.FloatField(null=True, blank=True, verbose_name='Точность')
+    bearing = models.FloatField(null=True, blank=True, verbose_name='Азимут')
+    timestamp = models.DateTimeField(verbose_name='Время')
+    class Meta:
+        verbose_name_plural = 'Треки' 
+        verbose_name = 'POI' 
+    
+ 
--- listenports/views.py
+++ listenports/views.py
@@ -1,17 +1,23 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse, HttpResponseForbidden
 from django.views.generic.base import View
 from datetime import datetime
 from django.utils import timezone
 import requests
 
-from listenports.models import Tracks
+from listenports.models import Tracks, Trackers
 
 class ListenPortView(View):
     def post(self, request):
+        try:
+            tracker = Trackers.objects.get(tracker_id=request.GET['id'])
+        except:
+            return HttpResponseForbidden()
+
         dt = datetime.fromtimestamp(int(request.GET['timestamp']))
         new_datetime = timezone.make_aware(dt, timezone.utc)       
         gps_point = Tracks()
         gps_point.tracker_id = request.GET['id']
+        gps_point.tracker_m_id = tracker.id
         gps_point.lon = request.GET['lon']
         gps_point.lat = request.GET['lat']
         gps_point.alt = request.GET['altitude']
@@ -21,21 +27,22 @@ class ListenPortView(View):
         gps_point.timestamp = new_datetime
         gps_point.save()
 
-        post_data = {
-            'id': request.GET['id'],
-            'timestamp': request.GET['timestamp'],
-            'lon': request.GET['lon'],
-            'lat': request.GET['lat'],
-            'altitude': request.GET['altitude'],
-            'speed': request.GET['speed'],
-            'accuracy': request.GET['accuracy'],
-            'bearing': request.GET['bearing'],
-            'batt': request.GET['batt'],
-        }
-        _headers = {'User-Agent':'TraccarClient'}
-        response = requests.post('http://livegpstracks.com', params=post_data, headers=_headers)
-        #response = requests.post('http://127.0.0.1:8000', params=post_data)
-        content = response.content
+        if tracker.resend:
+            post_data = {
+                'id': request.GET['id'],
+                'timestamp': request.GET['timestamp'],
+                'lon': request.GET['lon'],
+                'lat': request.GET['lat'],
+                'altitude': request.GET['altitude'],
+                'speed': request.GET['speed'],
+                'accuracy': request.GET['accuracy'],
+                'bearing': request.GET['bearing'],
+                'batt': request.GET['batt'],
+            }
+            _headers = {'User-Agent':'TraccarClient'}
+            response = requests.post('http://livegpstracks.com', params=post_data, headers=_headers)
+            #response = requests.post('http://127.0.0.1:8000', params=post_data)
+            content = response.content
 
         return HttpResponse()
 
