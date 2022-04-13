9958545aeac70c912fa46b9aaa7bd5ca8a750ad9
Vitaly Obrubov <obrubov@yandex.ru>
2022-04-13 19:48:18 +0300

    1

--- listenports/views.py
+++ listenports/views.py
@@ -1,21 +1,34 @@
+from typing import Dict
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.generic.base import View
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils import timezone
 import requests
+from geopy.distance import geodesic as GD
 
 from listenports.models import Tracks, Trackers
+last_positions: Dict[int, Dict[str,float]] = {}
 
-
+def bad_move(last_pos,new_pos):
+    time = (new_pos['time']-last_pos['time']).total_seconds()
+    distance = GD((last_pos['lat'],last_pos['lon']), (new_pos['lat'],new_pos['lon'])).km
+    speed = distance*1000/time
+    return speed > 60
+    
 class ListenPortView(View):
     def post(self, request):
         try:
             tracker = Trackers.objects.get(tracker_id=request.GET['id'])
         except:
             return HttpResponseForbidden()
-
         dt = datetime.fromtimestamp(int(request.GET['timestamp']))
         new_datetime = timezone.make_aware(dt, timezone.utc)
+        last_pos = last_positions.get(request.GET['id'])
+        new_pos = {'lat':request.GET['lat'], 'lon':request.GET['lon'], 'time':new_datetime}
+        if last_pos:
+            if bad_move(last_pos,new_pos):
+                return HttpResponse()
+        last_positions[request.GET['id']] = new_pos
         gps_point = Tracks()
         gps_point.tracker_id = request.GET['id']
         gps_point.tracker_m_id = tracker.id
