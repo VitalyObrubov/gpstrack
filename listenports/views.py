4a141c9ccf11d67dd59a00301f60e54f0ac957a9
Vitaly Obrubov <obrubov@yandex.ru>
2022-04-13 20:44:22 +0300

    1

--- listenports/views.py
+++ listenports/views.py
@@ -9,7 +9,7 @@ from geopy.distance import geodesic as GD
 from listenports.models import Tracks, Trackers
 last_positions: Dict[int, Dict[str,float]] = {}
 
-def bad_move(last_pos,new_pos):
+def bad_speed(last_pos,new_pos):
     time = (new_pos['time']-last_pos['time']).total_seconds()
     distance = GD((last_pos['lat'],last_pos['lon']), (new_pos['lat'],new_pos['lon'])).km
     speed = distance*1000/time
@@ -23,10 +23,14 @@ class ListenPortView(View):
             return HttpResponseForbidden()
         dt = datetime.fromtimestamp(int(request.GET['timestamp']))
         new_datetime = timezone.make_aware(dt, timezone.utc)
+        # Отбрасываем с низкой точностью
+        if request.GET['accuracy']>20:
+            return HttpResponse()
+        #  Отбрасываем если нереальная скорость 
         last_pos = last_positions.get(request.GET['id'])
         new_pos = {'lat':request.GET['lat'], 'lon':request.GET['lon'], 'time':new_datetime}
         if last_pos:
-            if bad_move(last_pos,new_pos):
+            if bad_speed(last_pos,new_pos):
                 return HttpResponse()
         last_positions[request.GET['id']] = new_pos
         gps_point = Tracks()
