27a3a482b69998bebe1e2df805d63505e112914f
Vitaly Obrubov <obrubov@yandex.ru>
2022-04-14 08:27:08 +0300

    1

--- listenports/views.py
+++ listenports/views.py
@@ -7,14 +7,17 @@ import requests
 from geopy.distance import geodesic as GD
 
 from listenports.models import Tracks, Trackers
-last_positions: Dict[int, Dict[str,float]] = {}
+last_positions: Dict[int, Dict[str, float]] = {}
 
-def bad_speed(last_pos,new_pos):
+
+def bad_speed(last_pos, new_pos):
     time = (new_pos['time']-last_pos['time']).total_seconds()
-    distance = GD((last_pos['lat'],last_pos['lon']), (new_pos['lat'],new_pos['lon'])).km
+    distance = GD((last_pos['lat'], last_pos['lon']),
+                  (new_pos['lat'], new_pos['lon'])).km
     speed = distance*1000/time
     return speed > 60
-    
+
+
 class ListenPortView(View):
     def post(self, request):
         try:
@@ -24,13 +27,14 @@ class ListenPortView(View):
         dt = datetime.fromtimestamp(int(request.GET['timestamp']))
         new_datetime = timezone.make_aware(dt, timezone.utc)
         # Отбрасываем с низкой точностью
-        if float(request.GET['accuracy'])>20:
+        if float(request.GET['accuracy']) > 20:
             return HttpResponse()
-        #  Отбрасываем если нереальная скорость 
+        #  Отбрасываем если нереальная скорость
         last_pos = last_positions.get(request.GET['id'])
-        new_pos = {'lat':request.GET['lat'], 'lon':request.GET['lon'], 'time':new_datetime}
+        new_pos = {'lat': request.GET['lat'],
+                   'lon': request.GET['lon'], 'time': new_datetime}
         if last_pos:
-            if bad_speed(last_pos,new_pos):
+            if bad_speed(last_pos, new_pos):
                 return HttpResponse()
         last_positions[request.GET['id']] = new_pos
         gps_point = Tracks()
